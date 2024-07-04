572a456d884560222f8dd4349eab74e4330e1eca
Daniel <danielzhang93@163.com>
2024-07-04 10:53:43 +0800

    update

--- SeleniumWebDriver/iFrame_test.py
+++ SeleniumWebDriver/iFrame_test.py
@@ -4,21 +4,17 @@ from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 def mail_login():
-    global webdriver
-    options = webdriver.ChromeOptions()  # 浏览器可选项配置 FirefoxOption
-    options.add_argument('--headless')  # 添加无头模式的参数
+    # options = webdriver.ChromeOptions()  # 浏览器可选项配置 FirefoxOption
+    # options.add_argument('--headless')  # 添加无头模式的参数
     driver = webdriver.Chrome()
     driver.get('https://mail.qq.com/')
     #
-    # time.sleep(10)
-    # driver.switch_to.frame(driver.find_elements(By.CLASS_NAME, "QQMailSdkTool_login_loginBox_qq_iframe")[0])
-    #
-    # driver.quit()
 
     """
     切换frame
@@ -35,37 +31,58 @@ def mail_login():
                 driver.switch_to.window('main')
     
     """
-    try:
-        # 等待iframe出现
-        wait = WebDriverWait(driver, 10)
-        iframe = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "QQMailSdkTool_login_loginBox_qq_iframe")))
-
-        # 切换到iframe
-        driver.switch_to.frame(iframe)
-
-        # 检查是否成功切换到iframe
-        try:
-            # 尝试查找iframe中的元素
-            # driver.find_element(By.ID, "switcher_plogin").click()
-
-            element_in_iframe = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="u"]')))
-            # driver.find_element(By.XPATH, '//*[@id="u"]').send_keys('305332527'))
-            print(f"Successfully switched to iframe and found element: {element_in_iframe.text}")
-        except Exception as e:
-            print(f"Failed to find element in iframe: {e}")
-
-        # 切回主内容
-        driver.switch_to.default_content()
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-    driver.switch_to.frame('ptlogin_iframe')
-    driver.switch_to.frame(1)
-    driver.switch_to.frame(driver.find_elements(By.XPATH, '//*[@id="QQMailSdkTool_login_loginBox_qq"]'))
-    driver.find_element(By.ID, 'u').send_keys('305332527')
-
+    time.sleep(10)
+    driver.switch_to.frame(driver.find_elements(By.CLASS_NAME, "QQMailSdkTool_login_loginBox_qq_iframe")[0])
+    driver.switch_to.frame(driver.find_elements(By.ID, 'ptlogin_iframe')[0])
+    driver.find_element(By.ID, "switcher_plogin").click()
     driver.find_element(By.XPATH, "//*[@id='u']").send_keys('305332527')
+    # driver.find_element(By.ID, "u").send_keys(111111)  # 输入用户名
+    driver.find_element(By.ID, "p").send_keys('ZXF93031')  # 输入密码
+    driver.find_element(By.ID, "login_button").click() # 点击登录按钮
+
+    # driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="QQMailSdkTool_login_loginBox_qq"]/iframe'))
+    # 切换到第二层frame
+    # driver.switch_to.frame('ptlogin_iframe')
+    # driver.find_element(By.ID, 'switcher_plogin').click()  # 点击密码登录按钮
+
+    time.sleep(3)
+
+
+    # driver.switch_to.frame(driver.find_elements(By.XPATH, '//*[@id="QQMailSdkTool_login_loginBox_qq"]'))
+    # try:
+    #     # 等待iframe出现
+    #     wait = WebDriverWait(driver, 10)
+    #     iframe = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "QQMailSdkTool_login_loginBox_qq_iframe")))
+    #
+    #     # 切换到iframe
+    #     driver.switch_to.frame(iframe)
+    #
+    #     # 检查是否成功切换到iframe
+    #     try:
+    #         # 尝试查找iframe中的元素
+    #         #
+    #
+    #         element_in_iframe = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="u"]')))
+    #         # driver.find_element(By.XPATH, '//*[@id="u"]').send_keys('305332527'))
+    #         print(f"Successfully switched to iframe and found element: {element_in_iframe.text}")
+    #     except Exception as e:
+    #         print(f"Failed to find element in iframe: {e}")
+    #
+    #     # 切回主内容
+    #     driver.switch_to.default_content()
+    #
+    # except Exception as e:
+    #     print(f"Error: {e}")
+
+    # driver.switch_to.frame('ptlogin_iframe')
+    # driver.switch_to.frame(1)
+
+    # driver.find_element(By.ID, 'u').send_keys('305332527')
+    #
+
+
+    # driver.close()
+    input("Selenium running done.")
 
 
 if __name__ == '__main__':
