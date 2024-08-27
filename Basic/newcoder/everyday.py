82f5eec32c820e15a3a2119c29b48db7f05b7f00
Xiaofei Zhang <xiaofeizhang@XiaofeideMacBook-Pro.local>
2024-08-27 08:03:34 +0800

    update

---
+++ Basic/newcoder/everyday.py
@@ -0,0 +1,33 @@
+import os
+
+"""
+
+"""
+a = "hello"
+b = a.capitalize()
+# print(b)
+
+
+def func(*args, **kwargs):
+    print(args)
+    print(kwargs)
+
+
+"""
+    
+"""
+
+
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print('{}*{}={}'.format(j, i, i * j,),end='')
+    print(end='\n')
+
+# def buble_sort(arr):
+#     for i in range(len(arr)):
+#         print(i)
+
+
+# if __name__ == "__main__":
+    # func(av=1, ab=2)
+    # buble_sort()
